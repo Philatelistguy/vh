4dbca787e6112c88a4dba40fe07f8fac432876fe
Anish Nayak <anish1.nayak@gmail.com>
2023-03-27 11:05:36 +0530

    button

--- components/Header.jsx
+++ components/Header.jsx
@@ -77,7 +77,7 @@ const handleClose =()=> setNav(!click)
                   </svg>                  
                 </button>
               </li>):<li>
-                <button className="btn-sm w-40 text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3" onClick={login}>
+                <button className="btn-sm w-40 text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3 w-44" onClick={login}>
                   <span>Login / Sign Up</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
